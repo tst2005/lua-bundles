64590ebb861cb7a8efa391a37dd0a398174b0c98
TsT <tst2005@gmail.com>
2017-11-10 13:35:35 +0100

    install script

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+
 set -e
-[ -d ~/.lua-bundles ] || git clone https://github.com/tst2005/generated-bundle ~/.lua-bundles
+
+[ -d ~/.lua-sword ] || git clone https://github.com/tst2005/generated-bundle ~/.lua-bundles
 cd ~/.lua-bundles
+git pull
+
 git clone https://github.com/tst2005/lua-mini
+
 [ -d mini ] || ln -s lua-mini/mini mini
 [ -f tprint.lua ] || ln -s mini/tprint.lua tprint.lua
 
