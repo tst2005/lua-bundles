1491e9a7f977936bf62a6292e9e31efe689709f6
TsT <tst2005@gmail.com>
2018-02-16 17:39:53 +0100

    update

--- install.sh
+++ install.sh
@@ -2,17 +2,19 @@
 
 set -e
 
-[ -d ~/.lua-sword ] || git clone https://github.com/tst2005/generated-bundle ~/.lua-bundles
+[ -d ~/.lua-bundles ] || git clone https://github.com/tst2005/generated-bundle ~/.lua-bundles
 cd ~/.lua-bundles
 git pull
 
 git clone https://github.com/tst2005/lua-mini
+cd lua-mini && git checkout dev && cd ..
 
 [ -d mini ] || ln -s lua-mini/mini mini
 [ -f tprint.lua ] || ln -s mini/tprint.lua tprint.lua
 
 git clone https://github.com/Yonaba/Moses lua-moses ## tag ?
 ln -s lua-moses/moses_min.lua moses.lua
+
 
 
 # export LUA_PATH="${HOME:-~}/.lua-bundles/?.lua;;"
